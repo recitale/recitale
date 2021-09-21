5f9ed2c62883d7b1b45428c0dc2dd00e2c6c2752
Quentin Schulz <foss@0leil.net>
2021-09-21 22:25:09 +0200

    cache: save cache also when Ctrl+C/SIGINT/KeyboardInterrupt is sent
    The Manager server process started when calling Manager() is not able to
    communicate with the main process when it's received a SIGINT making it
    impossible to dump the cache in that event.
    
    To allow graceful shutdowns, let's make the Manager server process
    ignore the SIGINT signal and let the other processes raise the proper
    exception so that in the try:catch the cache can be saved before
    "crashing".
    
    Shamelessly stolen from python-nose[1].
    
    [1] https://github.com/nose-devs/nose/blob/master/nose/plugins/multiprocess.py#L144-L153
    
    Signed-off-by: Quentin Schulz <foss@0leil.net>

--- prosopopee/cache.py
+++ prosopopee/cache.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import signal
 
 from multiprocessing import Manager
 
@@ -30,7 +31,12 @@ class Cache:
             print("info: cache format as changed, prune cache")
             cache = {"version": CACHE_VERSION}
 
+        # Make the Manager server process ignore the SIGINT (Ctrl+, aka KeyboardInterrupt exception)
+        # so that it is possible to still dump the cache variable after a KeyboardInterrupt.
+        old = signal.signal(signal.SIGINT, signal.SIG_IGN)
         self.cache = Manager().dict(cache)
+        # Current process should handle SIGINT as it used to do before starting the Manager process.
+        signal.signal(signal.SIGINT, old)
 
     def needs_to_be_generated(self, source, target, options):
         if not os.path.exists(target):
