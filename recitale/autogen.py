recitale/recitale

commit 5e525a5d0750556770c9ffa86750b0a6ed2979b5

autogen: autofill date gallery setting if missing
This allows to not provide a 'date' setting for autogenerated
galleries. The 'date' will automatically be retrieved from the oldest
file in the gallery, either from one of its EXIF date tags or from its
mtime.

Suggested-by: crypto512 <crypto512x@gmail.com>
Signed-off-by: Quentin Schulz <foss+recitale@0leil.net>

recitale/autogen.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import sys
-from time import gmtime, strftime
+from time import gmtime, strftime, strptime
 from jinja2 import Template
 from pathlib import Path
 from PIL import Image
@@ -33,6 +33,8 @@ logger = logging.getLogger("recitale." + __name__)
 
 types = ("*.JPG", "*.jpg", "*.JPEG", "*.jpeg", "*.png", "*.PNG")
 
+TIME_FORMAT = "%Y:%m:%d %H:%M:00"
+
 
 def get_exif(filename):
     exif = Image.open(filename).getexif()
@@ -42,7 +44,7 @@ def get_exif(filename):
         if ctime is not None:
             return ctime
 
-    return strftime("%Y:%m:%d %H:%M:00", gmtime(os.path.getmtime(filename)))
+    return strftime(TIME_FORMAT, gmtime(os.path.getmtime(filename)))
 
 
 def build_template(folder, force):
@@ -58,10 +60,6 @@ def build_template(folder, force):
         logger.error("%s/settings.yaml: 'title' setting missing", folder)
         sys.exit(1)
 
-    if "date" not in gallery_settings:
-        logger.error("%s/settings.yaml: 'date' setting missing", folder)
-        sys.exit(1)
-
     if "sections" in gallery_settings and force is not True:
         logger.info("Skipped: %s gallery is already generated", folder)
         return
@@ -73,10 +71,14 @@ def build_template(folder, force):
     files = sorted(files_grabbed, key=get_exif)
 
     cover = gallery_settings.get("cover", files[0].name)
+    date = gallery_settings.get("date")
+    if not date:
+        date_from_exif = strptime(get_exif(files[0]), TIME_FORMAT)
+        date = strftime("%Y-%m-%d", date_from_exif)
 
     msg = template.render(
         title=gallery_settings["title"],
-        date=gallery_settings["date"],
+        date=date,
         cover=cover,
         files=files,
     )

test/test_autogen.py
@@ -132,15 +132,6 @@ class TestBuildTemplate:
         assert sysexit.type == SystemExit
         assert sysexit.value.code == 1
 
-    def test_missing_required_date(self, caplog):
-        with pytest.raises(SystemExit) as sysexit, patch(
-            "recitale.autogen.load_settings", return_value={"title": "test"}
-        ):
-            recitale.autogen.build_template(".", False)
-        assert ": 'date' setting missing" in caplog.text
-        assert sysexit.type == SystemExit
-        assert sysexit.value.code == 1
-
     @patch(
         "recitale.autogen.load_settings",
         return_value={
@@ -154,6 +145,32 @@ class TestBuildTemplate:
         caplog.set_level(logging.INFO)
         recitale.autogen.build_template(".", False)
         assert " gallery is already generated" in caplog.text
+
+    @patch(
+        "recitale.autogen.load_settings",
+        return_value={"title": "test", "cover": "test.jpg"},
+    )
+    @patch("recitale.autogen.get_exif", return_value="2023:06:10 10:10:00")
+    def test_missing_date(self, patch_exif, patch_load):
+        with TemporaryDirectory() as td:
+            f = "test.png"
+            Path(td).joinpath(f).touch()
+            recitale.autogen.build_template(td, False)
+            generated = Path(td).joinpath("settings.yaml")
+            assert generated.exists()
+            with open(generated) as content:
+                assert (
+                    "".join(content.readlines())
+                    == f"""title: test
+date: 2023-06-10
+cover: test.jpg
+sections:
+  - type: pictures-group
+    images:
+      -
+         - {f}
+"""
+                )
 
     @patch(
         "recitale.autogen.load_settings",

commit 3ca04fbe733ccf551d7695c6cd7b98f9ccc5d9e9

autogen: include seconds in date format
TIME_FORMAT is used both for encoding and decoding a date. If a media
file doesn't contain an exif field for the date, it'll be provided one
from the mtime, itself encoded via TIME_FORMAT. However if a media file
does contain an exif field for the date, then it'll be used without
going through the encoding part. Therefore, the decoding should take
into account that the seconds part of the date can be something else
than 00.

This fixes autogen for the oldest file, in a gallery without a predefined
cover, with an exif date field where the seconds part was non-zero.

Fixes: 5e525a5d0750 ("autogen: autofill date gallery setting if missing")
Signed-off-by: Quentin Schulz <foss+recitale@0leil.net>

recitale/autogen.py
@@ -33,7 +33,7 @@ logger = logging.getLogger("recitale." + __name__)
 
 types = ("*.JPG", "*.jpg", "*.JPEG", "*.jpeg", "*.png", "*.PNG")
 
-TIME_FORMAT = "%Y:%m:%d %H:%M:00"
+TIME_FORMAT = "%Y:%m:%d %H:%M:%S"
 
 
 def get_exif(filename):

test/test_autogen.py
@@ -296,14 +296,14 @@ class TestGetExif:
             p.return_value.getexif.return_value = None
             assert (
                 recitale.autogen.get_exif("example/first_gallery/stuff.png")
-                == "2021:10:27 19:24:00"
+                == "2021:10:27 19:24:08"
             )
 
     @patch("recitale.autogen.os.path.getmtime", return_value=1635362648.7638042)
     def test_no_datetime_exifs(self, patched_getmtime):
         assert (
             recitale.autogen.get_exif("example/first_gallery/stuff.png")
-            == "2021:10:27 19:24:00"
+            == "2021:10:27 19:24:08"
         )
 
     @pytest.mark.parametrize("exif", [0x9003, 0x9004, 0x0132])