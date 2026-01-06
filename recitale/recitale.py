772b48e0e90fca33de14349b2d92263d9d0cc5b0
Quentin Schulz <foss+recitale@0leil.net>
2026-01-06 12:13:01 +0100

    recitale: move content default licence out of themes
    The default licence of the content of the website shouldn't depend on
    the theme. So let's specify the default in code so that the theme only
    has to display the licence without additional fluff.
    
    Signed-off-by: Quentin Schulz <foss+recitale@0leil.net>

--- recitale/recitale.py
+++ recitale/recitale.py
@@ -69,6 +69,10 @@ SETTINGS = {
         "audio": "libmp3lame",
         "extension": "mp3",
     },
+    "licence": {
+        "name": "CC-BY-SA-3.0",
+        "url": "https://creativecommons.org/licenses/by-sa/3.0/",
+    },
 }
 
 
